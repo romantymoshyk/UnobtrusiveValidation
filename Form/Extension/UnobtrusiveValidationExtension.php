ae3369fdb2fed90e9d80841f3cced78c3d672794
Roman Tymoshyk <tymoshyk.roman@gmail.com>
2017-01-30 17:13:19 +0200

    Code cleanup and optimization

--- Form/Extension/UnobtrusiveValidationExtension.php
+++ Form/Extension/UnobtrusiveValidationExtension.php
@@ -16,11 +16,32 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
     protected $translator;
 
     /**
-     * @param TranslatorInterface $translator
+     * @var null|string
      */
-    public function setTranslator(TranslatorInterface $translator)
+    private $translationDomain;
+
+    /**
+     * @param TranslatorInterface $translator The translator for translating error messages
+     * @param null|string $translationDomain The translation domain for translating
+     */
+    public function __construct(TranslatorInterface $translator, $translationDomain = null)
     {
         $this->translator = $translator;
+        $this->translationDomain = $translationDomain;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults(
+            array(
+                'upload_max_size_message' => 'test'
+            )
+        );
     }
 
     /**
@@ -31,19 +52,21 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
         if (count($options['constraints']) > 0) {
             $view->vars['attr']['data-val'] = 'true';
 
+            $label = $this->trans($options['label']);
+
             /** @var Constraint $constraint */
             foreach ($options['constraints'] as $constraint) {
                 switch (get_class($constraint)) {
                     case 'Symfony\Component\Validator\Constraints\Required':
                         $view->vars['attr']['data-val-required'] = $this->trans(
                             'The \'{{ field_name }}\' field is required.',
-                            array('{{ field_name }}' => $this->trans($options['label']))
+                            array('{{ field_name }}' => $label)
                         );
                         break;
                     case 'Symfony\Component\Validator\Constraints\Regex':
                         $view->vars['attr']['data-val-regex'] = $this->trans(
                             'The \'{{ field_name }}\' value is not valid',
-                            array('{{ field_name }}' => $this->trans($options['label']))
+                            array('{{ field_name }}' => $label)
                         );
                         $view->vars['attr']['data-val-regex-pattern'] = $constraint->htmlPattern;
                         break;
@@ -53,7 +76,7 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                             $view->vars['attr']['data-val-range'] = $this->trans(
                                 'The field \'{{ field_name }}\' value should be {{ limit }} or more.',
                                 array(
-                                    '{{ field_name }}' => $this->trans($options['label']),
+                                    '{{ field_name }}' => $label,
                                     '{{ limit }}' => $constraint->min
                                 )
                             );
@@ -63,7 +86,7 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                             $view->vars['attr']['data-val-range'] = $this->trans(
                                 'The field \'{{ field_name }}\' value should be {{ limit }} or less.',
                                 array(
-                                    '{{ field_name }}' => $this->trans($options['label']),
+                                    '{{ field_name }}' => $label,
                                     '{{ limit }}' => $constraint->max
                                 )
                             );
@@ -73,7 +96,7 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                             $view->vars['attr']['data-val-range'] = $this->trans(
                                 'The field \'{{ field_name }}\' value should be in range {{ min }} to {{ max }}.',
                                 array(
-                                    '{{ field_name }}' => $this->trans($options['label']),
+                                    '{{ field_name }}' => $label,
                                     '{{ min }}' => $constraint->min,
                                     '{{ max }}' => $constraint->max
                                 )
@@ -86,7 +109,7 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                             $view->vars['attr']['data-val-length'] = $this->trans(
                                 'The field \'{{ field_name }}\' should have {{ limit }} or more.',
                                 array(
-                                    '{{ field_name }}' => $this->trans($options['label']),
+                                    '{{ field_name }}' => $label,
                                     '{{ limit }}' => $constraint->min
                                 )
                             );
@@ -97,7 +120,7 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                             $view->vars['attr']['data-val-length'] = $this->trans(
                                 'The field \'{{ field_name }}\' should have {{ limit }} or less.',
                                 array(
-                                    '{{ field_name }}' => $this->trans($options['label']),
+                                    '{{ field_name }}' => $label,
                                     '{{ limit }}' => $constraint->max
                                 )
                             );
@@ -107,7 +130,7 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                             $view->vars['attr']['data-val-length'] = $this->trans(
                                 'The field \'{{ field_name }}\' should have from {{ min }} to {{ max }} characters.',
                                 array(
-                                    '{{ field_name }}' => $this->trans($options['label']),
+                                    '{{ field_name }}' => $label,
                                     '{{ min }}' => $constraint->min,
                                     '{{ max }}' => $constraint->max
                                 )
@@ -121,7 +144,7 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                             case 'long':
                                 $view->vars['attr']['data-val-digits'] = $this->trans(
                                     'The \'{{ field_name }}\' field value is not a valid integer.',
-                                    array('{{ field_name }}' => $this->trans($options['label']))
+                                    array('{{ field_name }}' => $label)
                                 );
                                 break;
                             case 'float':
@@ -129,7 +152,7 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                             case 'real':
                                 $view->vars['attr']['data-val-number'] = $this->trans(
                                     'The \'{{ field_name }}\' field value is not a valid number.',
-                                    array('{{ field_name }}' => $this->trans($options['label']))
+                                    array('{{ field_name }}' => $label)
                                 );
                                 break;
                         }
@@ -137,38 +160,30 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
                     case 'Symfony\Component\Validator\Constraints\Date':
                         $view->vars['attr']['data-val-date'] = $this->trans(
                             'The \'{{ field_name }}\' field value is not a valid date.',
-                            array('{{ field_name }}' => $this->trans($options['label']))
+                            array('{{ field_name }}' => $label)
                         );
                         break;
                     case 'Symfony\Component\Validator\Constraints\Email':
                         $view->vars['attr']['data-val-email'] = $this->trans(
                             'The \'{{ field_name }}\' field value is not a valid email address.',
-                            array('{{ field_name }}' => $this->trans($options['label']))
+                            array('{{ field_name }}' => $label)
                         );
                         break;
                     case 'Symfony\Component\Validator\Constraints\CardScheme':
                         $view->vars['attr']['data-val-creditcard'] = $this->trans(
                             'The \'{{ field_name }}\' field value is not a valid credit card number.',
-                            array('{{ field_name }}' => $this->trans($options['label']))
+                            array('{{ field_name }}' => $label)
                         );
                         break;
                     case 'Symfony\Component\Validator\Constraints\Url':
                         $view->vars['attr']['data-val-url'] = $this->trans(
                             'The \'{{ field_name }}\' field value is not a valid url.',
-                            array('{{ field_name }}' => $this->trans($options['label']))
+                            array('{{ field_name }}' => $label)
                         );
                         break;
                 }
             }
         }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        parent::configureOptions($resolver);
     }
 
     /**
@@ -189,14 +204,12 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
      *
      * @return string The translated string
      */
-    /**  @noinspection MoreThanThreeArgumentsInspection */
-    public function trans($id, array $parameters = array(), $domain = null, $locale = null)
+    public function trans($id, array $parameters = array())
     {
-        return $this->translator !== null ? $this->translator->trans(
+        return $this->translator !== null && $this->translationDomain !== false ? $this->translator->trans(
             $id,
             $parameters,
-            $domain,
-            $locale
+            $this->translationDomain
         ) : $id;
     }
 
@@ -211,15 +224,13 @@ class UnobtrusiveValidationExtension extends AbstractTypeExtension
      *
      * @return string The translated string
      */
-    /**  @noinspection MoreThanThreeArgumentsInspection */
-    public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null)
+    public function transChoice($id, $number, array $parameters = array())
     {
-        return $this->translator !== null ? $this->translator->transChoice(
+        return $this->translator !== null && $this->translationDomain !== false ? $this->translator->transChoice(
             $id,
             $number,
             $parameters,
-            $domain,
-            $locale
+            $this->translationDomain
         ) : $id;
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-[![Build Status](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/badges/build.png?b=1.0)](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/build-status/1.0) [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/badges/quality-score.png?b=1.0)](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/?branch=1.0) [![Code Coverage](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/badges/coverage.png?b=1.0)](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/?branch=1.0)
-# unobtrusive-validation-bundle
-Let you add support of Unobtrusive Validation to Symfony projects
+[![Build Status](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/badges/build.png?b=1.0)](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/build-status/1.0) 
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/badges/quality-score.png?b=1.0)](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/?branch=1.0) 
+[![Code Coverage](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/badges/coverage.png?b=1.0)](https://scrutinizer-ci.com/g/romantymoshyk/unobtrusive-validation-bundle/?branch=1.0)
+[![Total Downloads](https://poser.pugx.org/romantymoshyk/unobtrusive-validation-bundle/downloads)](https://packagist.org/packages/romantymoshyk/unobtrusive-validation-bundle)
+[![License](https://poser.pugx.org/romantymoshyk/unobtrusive-validation-bundle/license)](https://packagist.org/packages/romantymoshyk/unobtrusive-validation-bundle)
+[![Latest Stable Version](https://poser.pugx.org/romantymoshyk/unobtrusive-validation-bundle/v/stable)](https://packagist.org/packages/romantymoshyk/unobtrusive-validation-bundle)
+[![Latest Unstable Version](https://poser.pugx.org/romantymoshyk/unobtrusive-validation-bundle/v/unstable)](https://packagist.org/packages/romantymoshyk/unobtrusive-validation-bundle)
+
+# Unobtrusive Validation Bundle
+
+This Symfony 3.x compatible Bundle let you add support of Unobtrusive Validation to your Symfony projects
+
+## Security Vulnerabilities
+
+If you discover a security vulnerability within UnobtrusiveValidationBundle, please send an e-mail. All security vulnerabilities will be promptly addressed.
+
+## License
+
+The UnobtrusiveValidationBundle is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
